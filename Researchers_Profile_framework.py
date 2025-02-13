a699486120ac15210fcdfc381ff6c56f5557ff54
MasilelaNdumiso <ndumiso.masileal1@gmail.com>
2025-02-13 14:20:15 +0200

    Add files via upload

--- Researchers_Profile_framework.py
+++ Researchers_Profile_framework.py
@@ -11,3 +11,9 @@ st.title("Researchers Profile")
 
 st.markdown("I am a dedicated and detail-oriented Hydrologist with a strong foundation in the National Water Act (Act 36 of 1998) with over three years of experience in South Africa’s national water monitoring programs. My expertise includes performance monitoring, drought risk management, and data management for the National Integrated Water Information System (NIWIS). I am a Professional Natural Scientist (SACNASP), proficient in hydrological modeling and flow routing using MIKE+ Rivers, MODFLOW, and HEC-RAS for water resources system operations, Geographical Information Systems, Satellite-based Remote Sensing, and advanced data management using R-Scripts and Python programming languages. My experience includes processing, analyzing, and interpreting various hydrological and spatial data, including expertise in High-Performance Computing (HPC) for large-scale data analysis. Additionally, I have co-authored peer-reviewed publications contributing to hydrological drought monitoring and groundwater assessment in South Africa.")
 
+
+tab1, tab2, tab3= st.tabs(["About", "Publications", "Current projects"])
+                                                               
+
+#%%
+#make the pages
